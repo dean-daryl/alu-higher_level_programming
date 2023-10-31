317289d1bdd37768f829266b86ed04afff844b45
dean-daryl <deandaryl7@gmail.com>
2023-10-31 17:26:58 +0200

    ��

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,7 +1,3 @@
 #!/usr/ bin/python3
-<<<<<<< HEAD
 print("Programming is like building a multilingual puzzle")
 
-=======
-print("Programming is like building a multilingual puzzle")
->>>>>>> 4334db70b5cfb6ab1f1ff891d645458d0ede4c57
