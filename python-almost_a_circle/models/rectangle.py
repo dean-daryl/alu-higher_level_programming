168800bd0326870004a2dece7daa63f117c7af10
dean-daryl <deandaryl7@gmail.com>
2023-12-13 17:44:09 +0200

    updated test files

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,9 +18,9 @@ class TestRectangleMethods(unittest.TestCase):
 
     def test_new_rectangle(self):
         """ Test all new rectangle """
-        new = Rectangle(1, 1)
+        new = Rectangle(1, 2)
         self.assertEqual(new.width, 1)
-        self.assertEqual(new.height, 1)
+        self.assertEqual(new.height, 2)
         self.assertEqual(new.x, 0)
         self.assertEqual(new.y, 0)
         self.assertEqual(new.id, 1)
