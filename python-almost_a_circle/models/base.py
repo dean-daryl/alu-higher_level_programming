b455f14e972f3d5f1883da3fe51518b84f36aecd
dean-daryl <deandaryl7@gmail.com>
2023-12-13 13:20:20 +0200

    returning an instance with all attributes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,35 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create an instance """
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        else:
+            new = cls(10)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """
+            loading dict representing the parameters for
+            and instance and from that creating instances
+        """
+        file_name = cls.__name__ + ".json"
+
+        try:
+            with open(file_name, encoding="UTF8") as fd:
+                content = cls.from_json_string(fd.read())
+        except:
+            return []
+
+        instances = []
+
+        for instance in content:
+            tmp = cls.create(**instance)
+            instances.append(tmp)
+
+        return instances
